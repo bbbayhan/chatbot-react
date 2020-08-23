72615f93a624bc30719da98b45f0edccd0050e08
Beste Burcu Bayhan <bestebbayhan@gmail.com>
2020-08-24 02:31:54 +0300

    Fix App margin and add to ChatTypeBar button

--- src/components/ChatBotContent/ChatTypeBar.jsx
+++ src/components/ChatBotContent/ChatTypeBar.jsx
@@ -1,14 +1,46 @@
 import React from "react";
-import { Input } from "antd";
+import { Input, Button } from "antd";
 
 import { SendOutlined } from "@ant-design/icons";
+import moment from "moment";
 
-export const ChatTypeBar = () => {
-  return (
-    <Input
-      size="large"
-      placeholder="Escribe tu pregunta"
-      suffix={<SendOutlined />}
-    />
-  );
-};
+export class ChatTypeBar extends React.Component {
+  state = {
+    messages: [],
+    userInput: "",
+    submitted: false,
+  };
+
+  onClick = () => {
+    if (!this.state.userInput) {
+      return;
+    }
+    this.setState({
+      submitted: true,
+    });
+    setTimeout(() => {
+      this.setState({
+        submitted: false,
+        userInput: "",
+        messages: [
+          {
+            datetime: moment().fromNow(),
+          },
+          ...this.state.messages,
+        ],
+      });
+    }, 1000);
+  };
+
+  render() {
+    //const { messages, submitted, userInput } = this.state;
+
+    return (
+      <Input
+        size="large"
+        placeholder="Escribe tu pregunta"
+        suffix={<Button icon={<SendOutlined />} onClick={this.onClick} />}
+      />
+    );
+  }
+}
